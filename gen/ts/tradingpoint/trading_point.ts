fc55586ad00126600e21ea459bfc2a894676f9cc
fatalistix <slavabal11@outlook.com>
2024-05-01 14:48:31 +0700

    regenerated files according to v0.0.2 proto

--- gen/ts/tradingpoint/trading_point.ts
+++ gen/ts/tradingpoint/trading_point.ts
@@ -11,7 +11,6 @@ import {
   type ServiceError,
   type UntypedServiceImplementation,
 } from "@grpc/grpc-js";
-import Long from "long";
 import _m0 from "protobufjs/minimal";
 import { Money } from "../core/money";
 
@@ -203,7 +202,7 @@ function createBaseRegisterResponse(): RegisterResponse {
 export const RegisterResponse = {
   encode(message: RegisterResponse, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
     if (message.id !== 0) {
-      writer.uint32(8).int64(message.id);
+      writer.uint32(8).int32(message.id);
     }
     if (message.type !== 0) {
       writer.uint32(16).int32(message.type);
@@ -223,7 +222,7 @@ export const RegisterResponse = {
             break;
           }
 
-          message.id = longToNumber(reader.int64() as Long);
+          message.id = reader.int32();
           continue;
         case 2:
           if (tag !== 16) {
@@ -325,18 +324,6 @@ export type DeepPartial<T> = T extends Builtin ? T
 type KeysOfUnion<T> = T extends T ? keyof T : never;
 export type Exact<P, I extends P> = P extends Builtin ? P
   : P & { [K in keyof P]: Exact<P[K], I[K]> } & { [K in Exclude<keyof I, KeysOfUnion<P>>]: never };
-
-function longToNumber(long: Long): number {
-  if (long.gt(globalThis.Number.MAX_SAFE_INTEGER)) {
-    throw new globalThis.Error("Value is larger than Number.MAX_SAFE_INTEGER");
-  }
-  return long.toNumber();
-}
-
-if (_m0.util.Long !== Long) {
-  _m0.util.Long = Long as any;
-  _m0.configure();
-}
 
 function isSet(value: any): boolean {
   return value !== null && value !== undefined;
